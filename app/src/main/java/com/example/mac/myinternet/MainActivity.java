572f0d0be912d639df81b5c163a1b9848b63f752
kk6665555 <49990115@stust.edu.tw>
2017-08-13 16:31:54 +0800

    0813

--- app/src/main/java/com/example/mac/myinternet/MainActivity.java
+++ app/src/main/java/com/example/mac/myinternet/MainActivity.java
@@ -1,5 +1,9 @@
 package com.example.mac.myinternet;
 
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.Handler;
+import android.os.Message;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -15,13 +19,19 @@ import java.net.URL;
 
 public class MainActivity extends AppCompatActivity {
     private ImageView img;
+    private Bitmap bmp;
+    private UI handler;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        img = (ImageView) findViewById(R.id.img);
+        handler=new UI();
     }
 
     public void test1(View view){
+        //網際網路 必須寫在執行緒
         new Thread(){
             @Override
             public void run() {
@@ -57,14 +67,23 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void run() {
                 try {
-                    URL url = new URL("");
+                    URL url = new URL("http://www.penghu-nsa.gov.tw/FileDownload/Album/Big/20161012162551758864338.jpg");
                     HttpURLConnection conn =(HttpURLConnection) url.openConnection();
                     conn.connect();
-
+                    bmp = BitmapFactory.decodeStream(conn.getInputStream());
+                    handler.sendEmptyMessage(0);
                 }catch (Exception e){
-
+                    Log.i("test",e.toString());
                 }
             }
         }.start();
+    }
+
+    private class UI extends Handler{
+        @Override
+        public void handleMessage(Message msg) {
+            super.handleMessage(msg);
+            img.setImageBitmap(bmp);
+        }
     }
 }
